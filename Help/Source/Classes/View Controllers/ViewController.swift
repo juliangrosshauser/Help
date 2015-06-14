2c7e4af1399148df2607edd8277a6b6058795a92
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-14 22:31:02 +0200

    Add Xcode project

---
+++ Help/Source/Classes/View Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Help
+//
+//  Created by Julian Grosshauser on 14/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ HelpTests/Sources/Classes/HelpTests.swift
@@ -0,0 +1,20 @@
+//
+//  HelpTests.swift
+//  HelpTests
+//
+//  Created by Julian Grosshauser on 14/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+
+class HelpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
